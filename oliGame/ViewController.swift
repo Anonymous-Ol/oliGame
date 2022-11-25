b2beb52769f7aef271a0966e0dfebc0ddc1ed17a
Anonymous-Ol <84883214+Anonymous-Ol@users.noreply.github.com>
2022-11-25 16:02:57 -0500

    Initial Commit

---
+++ oliGame/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  oliGame
+//
+//  Created by Oliver Crumrine on 11/25/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
